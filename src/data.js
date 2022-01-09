9e05c65c0c94d3b9d7e2742448228e38cbcfa756
Dipesh Ingle <dipeshingle1926@gmail.com>
2022-01-09 22:56:24 +0530

    changes

--- src/data.js
+++ src/data.js
@@ -5,40 +5,60 @@ import website2 from "./img/website2.png";
 import website3 from "./img/website3.png";
 import website4 from "./img/website4.png";
 import youtube from "./img/youtube.jpg.png";
+import modernuiux from "./img/modern uiux.png";
+import traveladvisor from "./img/traveladvisor.png";
+
+import quiz from "./img/quiz.png";
 export const products = [
   {
-    id: 1,
+    id: 0,
     img: entertainmenthub,
     link: "https://entertainment-hub-c28f9.web.app/",
   },
   {
+    id: 1,
+    img: modernuiux,
+    link: "https://modern-ui-ux.web.app/",
+  },
+  {
     id: 2,
-    img: youtube,
-    link: "https://not-13949.firebaseapp.com/",
+    img: traveladvisor,
+    link: "https://travel-advisor-309c0.web.app/",
   },
   {
     id: 3,
+    img: youtube,
+    link: "https://not-13949.web.app/",
+  },
+  {
+    id: 4,
+    img: quiz,
+    link: "https://quizmania-8a770.web.app/",
+  },
+
+  {
+    id: 6,
     img: reactlandingpage,
 
     link: "https://react-landing-page-a4370.web.app/",
   },
   {
-    id: 4,
+    id: 7,
     img: website1,
     link: "https://website-1-db90e.web.app/",
   },
   {
-    id: 5,
+    id: 8,
     img: website2,
     link: "https://website-2-da4d5.web.app/",
   },
   {
-    id: 6,
+    id: 9,
     img: website3,
     link: "https://website-3-2bb8a.web.app/",
   },
   {
-    id: 7,
+    id: 10,
     img: website4,
     link: "https://website-4-ecbac.web.app/",
   },
